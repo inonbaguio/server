ece7580a69c28a64057b2166b2096d7ae41c3cfe
Inon Baguio <inonbaguio@gmail.com>
2019-03-03 11:18:30 +0800

    integrated mongoose

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
 require('./services/passport');
 
 const app = express();
 
 require('./routes/authRoutes')(app);
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true
+});
 
 const PORT = process.env.PORT || 5000;
+console.log('Server started at http://localhost:' + PORT);
 app.listen(PORT);
