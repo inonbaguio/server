0c2eaa17b95adadd9f9bd71cb5789d488b880730
Inon Baguio <inonbaguio@gmail.com>
2019-03-03 12:02:06 +0800

    integrated saving of user in mongoose

--- models/User.js
+++ models/User.js
@@ -2,3 +2,10 @@ const mongoose = require('mongoose');
 const {
   Schema
 } = mongoose;
+
+const userSchema = new Schema({
+  googleId: String,
+  name: String
+});
+
+mongoose.model('users', userSchema);
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy({
@@ -9,14 +12,13 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken", accessToken);
-      console.log("refreshToken", refreshToken);
-      console.log("profile", profile);
-      console.log("done", done);
+      new User({
+        googleId: profile.id
+      }).save();
     }
   )
 );
 
 module.exports = {
-  
+
 }
